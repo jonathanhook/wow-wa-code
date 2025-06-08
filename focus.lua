0e2c833f592824a27cee968836113a6d09cc6aa8
Jonathan Hook <jonathanhook@gmail.com>
2025-06-08 21:23:24 +0100

    Bug fixes from testing

--- focus.lua
+++ focus.lua
@@ -14,26 +14,26 @@ function ()
     local focus = UnitPower("player", SPELL_POWER_FOCUS)
     
     -- global cooldown
-    aura_env.gcd = aura_env.IsGlobalCooldownActive()
+    g_gcd = IsGlobalCooldownActive()
     if g_gcd then
         return
     end
    
     -- bestial wrath (if off cooldown)
-    if aura_env.IsSpellOffCooldown("Bestial Wrath") then
+    if IsSpellOffCooldown("Bestial Wrath") then
         g_bestialWrath = true
         return
     end
     
     -- dire beast (if off cooldown)
-    if aura_env.IsSpellOffCooldown("Dire Beast") then
+    if IsSpellOffCooldown("Dire Beast") then
         g_direBeast = true
         return
     end
 
-    -- barbed shot (if the next kill command will not proc Howl of the Pack Leader, we have a charge ready and Frenzy isn't already stacked to 3)
-    if not (aura_env.UnitHasBuff("player", "Howl of the Pack Leader") and focus >= 30) and aura_env.HasSpellCharges("Barbed Shot", 1) then
-        local buffData = aura_env.UnitHasBuff("pet", "Frenzy")
+    -- barbed shot (is off cooldown and we don't have three stacks of frenzy)
+    if IsSpellOffCooldown("Barbed Shot") and HasSpellCharges("Barbed Shot", 1) then
+        local buffData = UnitHasBuff("pet", "Frenzy")
         if not buffData or buffData.applications < 3 then
             g_barbedShot = true
             return
@@ -41,13 +41,13 @@ function ()
     end
 
     -- multi-shot (if we have enough focus, more than one enemy is engaged and beast cleave is not active)
-    if focus >= 40 and aura_env.IsMoreThanOneEnemyEngaged() and not aura_env.UnitHasBuff("pet", "Beast Cleave") then
+    if focus >= 40 and IsMoreThanOneEnemyEngaged() and not UnitHasBuff("pet", "Beast Cleave") then
         g_multiShot = true
         return
     end
            
     -- kill command (if we have enough focus and it is off cooldown)
-    if focus >= 30 and aura_env.IsSpellOffCooldown("Kill Command") then
+    if focus >= 30 and IsSpellOffCooldown("Kill Command") and HasSpellCharges("Kill Command", 1) then
         g_killCommand = true
         return
     end
